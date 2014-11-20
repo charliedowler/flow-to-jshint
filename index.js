120a8ecc2c218e9cdd20e8ba021e4f307927fd0c
charliedowler <charliedowler15@gmail.com>
2014-11-20 19:06:19 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var path = require('path');
+module.exports = function(result) {
+  var errors = result.errors;
+  var parsed = [];
+  errors.forEach(function(error) {
+    error.message.forEach(function(message) {
+      var report = {};
+      report.error = {
+        reason: message.descr.split('\n').join(' - '),
+        code: message.code ? 'W' : 'E',
+        character: message.start,
+        line: message.line
+      };
+      report.file = path.basename(message.path);
+      parsed.push(report);
+    });
+  });
+  return parsed;
+};
