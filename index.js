f4d2020f4447c507eda051abe938352bcb915ad5
charliedowler <charliedowler15@gmail.com>
2014-11-21 21:22:14 +0000

    show full path instead of filename

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = function(result) {
         character: message.start,
         line: message.line
       };
-      report.file = path.basename(message.path);
+      report.file = message.path;
       parsed.push(report);
     });
   });
